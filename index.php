2654aa5ce0b44e579bb3694eca35c0af6fb57e48
Luis mayta <luis.mayta@gmail.com>
2019-02-20 10:26:08 -0500

    Crear index y buscar

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+require 'vendor/autoload.php';
+
+
+$hosts = [
+    '94.130.73.248:9200',         // IP + Port
+];
+
+$client = Elasticsearch\ClientBuilder::create()           // Instantiate a new ClientBuilder
+                    ->setHosts($hosts)      // Set the hosts
+                    ->build();
+// if ($client) {
+//     echo 'connected';
+// }
+
+// $params = [
+//     'index' => 'my_index',
+//     'type' => 'my_type',
+//     'body' => [
+//         'nombre' => 'Bianca Valery',
+//         'apellido' => 'Mayta campos',
+//         'edad' => '40',
+//     ],
+//    ];
+#$response = $client->index($params);
+#print_r($response);
+
+$params['index'] = 'my_index';
+$params['type'] = 'my_type';
+$params['body']['query']['match']['nombre'] = 'bianca';
+
+$result = $client->search($params);
+print_r($result);
